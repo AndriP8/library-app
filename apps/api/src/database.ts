43cb9abafc225d6bdb0b481c33d2d8f33c7d9db1
Andri Purnomo <andri.adrp@gmail.com>
2024-08-23 21:43:28 +0700

    chore: setup docker for postgres

--- apps/api/src/database.ts
+++ apps/api/src/database.ts
@@ -8,6 +8,7 @@ const dialect = new PostgresDialect({
     host: process.env.DATABASE_HOST,
     user: process.env.DATABASE_USER,
     port: Number(process.env.DATABASE_PORT),
+    password: process.env.DATABASE_PASSWORD,
   }),
 });
 
