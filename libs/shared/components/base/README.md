3db3fc174e5713bf9c82c15abdb35202a3647d52
Andri Purnomo <andri.adrp@gmail.com>
2024-10-10 14:36:29 +0700

    chore: create libs for shared component base and insert useful components

---
+++ libs/shared/components/base/README.md
@@ -0,0 +1,11 @@
+# shared-components-base
+
+This library was generated with [Nx](https://nx.dev).
+
+## Building
+
+Run `nx build shared-components-base` to build the library.
+
+## Running unit tests
+
+Run `nx test shared-components-base` to execute the unit tests via [Jest](https://jestjs.io).
---
+++ libs/shared/components/base/tailwind.config.js
@@ -0,0 +1,19 @@
+const { createGlobPatternsForDependencies } = require('@nx/react/tailwind');
+const { join } = require('path');
+
+/** @type {import('tailwindcss').Config} */
+const tailwindConfig = {
+  presets: [require('../../../../tailwind.preset.js')],
+  content: [
+    join(
+      __dirname,
+      '{src,pages,components,app}/**/*!(*.stories|*.spec).{ts,tsx,html}'
+    ),
+    ...createGlobPatternsForDependencies(__dirname),
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+module.exports = tailwindConfig;
