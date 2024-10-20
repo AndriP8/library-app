fad49c4dc2d7585f5ac58757d1efc4e1dbf9f348
Andri Purnomo <andri.adrp@gmail.com>
2024-10-20 14:35:36 +0700

    chore: create sidebar component

--- tailwind.preset.js
+++ tailwind.preset.js
@@ -45,6 +45,20 @@ module.exports = {
           DEFAULT: 'hsl(var(--card))',
           foreground: 'hsl(var(--card-foreground))',
         },
+        sidebar: {
+          DEFAULT: 'hsl(var(--sidebar-background))',
+          foreground: 'hsl(var(--sidebar-foreground))',
+          accent: {
+            DEFAULT: 'hsl(var(--sidebar-accent))',
+            foreground: 'hsl(var(--sidebar-accent-foreground))',
+          },
+          border: {
+            DEFAULT: 'hsl(var(--sidebar-border))',
+          },
+          ring: {
+            DEFAULT: 'hsl(var(--sidebar-ring))',
+          },
+        },
       },
       borderRadius: {
         lg: `var(--radius)`,
