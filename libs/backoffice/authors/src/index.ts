157a381ce724edd0422df26d97c3a9c1de93a06b
Andri Purnomo <andri.adrp@gmail.com>
2024-10-27 14:51:35 +0700

    fix: conflicting star export

--- libs/backoffice/authors/src/index.ts
+++ libs/backoffice/authors/src/index.ts
@@ -1,4 +1,4 @@
 // Use this file to export React client components (e.g. those with 'use client' directive) or other non-server utilities
 
-export * from './lib/add-author/backoffice-add-author';
-export * from './lib/backoffice-authors';
+export { BackofficeAddAuthors } from './lib/add-author/backoffice-add-author';
+export { BackofficeAuthors } from './lib/backoffice-authors';
